911fc9d5a82bbdc7029b39fea70b878e762d00d3
Zachary White <zwhite@pgusd.org>
2025-10-28 14:19:38 -0700

    [1.0.1] Version 1.0.1 updates.

---
+++ pack-extension.sh
@@ -0,0 +1,3 @@
+# Git Bash:
+cd ~/Documents/git/chrome-extension-disable-colab-ai
+"/c/Program Files/Google/Chrome/Application/chrome.exe" --pack-extension="$(pwd)/extension" --pack-extension-key="$(pwd)/extension.pem"
